25ae2e5fcaca0a0975c780f9cdfb9d0be89c6d70
wdev733 <wdev733@gmail.com>
2017-01-10 02:40:58 +0100

    more endpoint testing

--- test/panel_get.js
+++ test/panel_get.js
@@ -7,53 +7,77 @@ var should = chai.should();
 
 chai.use(chaiHttp);
 
-describe("Checking Gets", function () {
-    it("get /", function (done) {
-        chai.request(server)
-        .get("/")
-        .end(function (err, res) {
-            if (err)
-                done(err);
-            res.status.should.equal(200);
-            done();
+describe("Check endpoints", function () {
+    describe("not authenticated", function () {
+        it("get /", function (done) {
+            chai.request(server)
+            .get("/")
+            .end(function (err, res) {
+                if (err)
+                    done(err);
+                res.status.should.equal(200);
+                done();
+            });
         });
-    });
-    it("get /home", function (done) {
-        chai.request(server)
-        .get("/home")
-        .end(function (err, res) {
-            if (err)
-                done(err);
-            res.status.should.equal(200);
-            done();
+        it("get /home", function (done) {
+            chai.request(server)
+            .get("/home")
+            .end(function (err, res) {
+                if (err)
+                    done(err);
+                res.status.should.equal(200);
+                done();
+            });
         });
-    });
-    it("get /login", function (done) {
-        chai.request(server)
-        .get("/login")
-        .end(function (err, res) {
-            if (err)
-                done(err);
-            res.status.should.equal(200);
-            done();
+        it("get /login", function (done) {
+            chai.request(server)
+            .get("/login")
+            .end(function (err, res) {
+                if (err)
+                    done(err);
+                res.status.should.equal(200);
+                done();
+            });
         });
-    });
-    it("get /logout", function (done) {
-        chai.request(server)
-        .get("/logout")
-        .end(function (err, res) {
-            if (err)
-                done(err);
-            res.status.should.equal(200);
-            done();
+        it("get /logout", function (done) {
+            chai.request(server)
+            .get("/logout")
+            .end(function (err, res) {
+                if (err)
+                    done(err);
+                res.status.should.equal(200);
+                done();
+            });
         });
-    });
-    it("get /api/list", function (done) {
-        chai.request(server)
-        .get("/api/list")
-        .end(function (err, res) {
-			res.status.should.equal(401);
-            done();
+        describe("api endpoints", function () {
+            it("get /api/list", function (done) {
+                chai.request(server)
+                .get("/api/list")
+                .end(function (err, res) {
+                    res.status.should.equal(401);
+                    done();
+                });
+            });
+            it("get /api/data?overTimeData10mins", function (done) {
+                chai.request(server)
+                .get("/api/data?overTimeData10mins")
+                .end(function (err, res) {
+                    if (err)
+                        done(err);
+                    res.status.should.equal(200);
+                    done();
+                });
+            });
+            it("get /api/data?summary", function (done) {
+                chai.request(server)
+                .get("/api/data?summary")
+                .end(function (err, res) {
+                    if (err)
+                        done(err);
+                    res.status.should.equal(200);
+                    done();
+                });
+            });
         });
     });
 });
