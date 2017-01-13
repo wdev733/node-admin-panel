e1cfc05e9514958bf87dc208baaece54ff93f2e1
wdev733 <wdev733@gmail.com>
2017-01-13 16:58:46 +0100

    added polyfill for Array.prototype.inclues

--- routes/api.js
+++ routes/api.js
@@ -31,6 +31,55 @@ if (typeof Object.assign != 'function') {
         return target;
     };
 }
+/////
+// Source: https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Array/includes
+if (!Array.prototype.includes) {
+    Object.defineProperty(Array.prototype, 'includes', {
+        value: function(searchElement, fromIndex) {
+
+            // 1. Let O be ? ToObject(this value).
+            if (this == null) {
+                throw new TypeError('"this" is null or not defined');
+            }
+
+            var o = Object(this);
+
+            // 2. Let len be ? ToLength(? Get(O, "length")).
+            var len = o.length >>> 0;
+
+            // 3. If len is 0, return false.
+            if (len === 0) {
+                return false;
+            }
+
+            // 4. Let n be ? ToInteger(fromIndex).
+            //    (If fromIndex is undefined, this step produces the value 0.)
+            var n = fromIndex | 0;
+
+            // 5. If n ≥ 0, then
+            //  a. Let k be n.
+            // 6. Else n < 0,
+            //  a. Let k be len + n.
+            //  b. If k < 0, let k be 0.
+            var k = Math.max(n >= 0 ? n : len - Math.abs(n), 0);
+
+            // 7. Repeat, while k < len
+            while (k < len) {
+                // a. Let elementK be the result of ? Get(O, ! ToString(k)).
+                // b. If SameValueZero(searchElement, elementK) is true, return true.
+                // c. Increase k by 1.
+                // NOTE: === provides the correct "SameValueZero" comparison needed here.
+                if (o[k] === searchElement) {
+                    return true;
+                }
+                k++;
+            }
+
+            // 8. Return false
+            return false;
+        }
+    });
+}
 /////////////////////////////////////////////
 
 router.get("/data", function(req, res) {
@@ -76,7 +125,7 @@ router.get("/data", function(req, res) {
     if ("getAllQueries" in req.query) {
         promises.push(logHelper.getAllQueries());
     }
-	console.log(Promise);
+    console.log(Promise);
     Promise.all(promises)
         .then(function(values) {
             var data = {};
