e90f44130fdaaf24a1c74ee9f07a12fa7a4b8d33
wdev733 <wdev733@gmail.com>
2017-01-13 16:23:39 +0100

    possible fix

--- routes/api.js
+++ routes/api.js
@@ -8,6 +8,32 @@ var router = express.Router();
 
 const supportedDataQueries = ["summary", "summaryRaw", "overTimeData", "overTimeData10mins", "topItems", "recentItems", "getQueryTypes", "getForwardDestinations"];
 
+// Potential buildfail fix for node 5 and below
+// polyfill source: https://developer.mozilla.org/de/docs/Web/JavaScript/Reference/Global_Objects/Object/assign
+if (typeof Object.assign != 'function') {
+  Object.assign = function(target) {
+    'use strict';
+    if (target == null) {
+      throw new TypeError('Cannot convert undefined or null to object');
+    }
+
+    target = Object(target);
+    for (var index = 1; index < arguments.length; index++) {
+      var source = arguments[index];
+      if (source != null) {
+        for (var key in source) {
+          if (Object.prototype.hasOwnProperty.call(source, key)) {
+            target[key] = source[key];
+          }
+        }
+      }
+    }
+    return target;
+  };
+}
+/////////////////////////////////////////////
+
+
 router.get("/data", function(req, res) {
     // Filter query types so only valid ones pass
     var args = {};
