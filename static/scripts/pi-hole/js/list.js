b8135d3477a5ff47475e207d71cee17360d6c504
wdev733 <wdev733@gmail.com>
2017-01-18 14:47:46 +0100

    list.js mods

--- static/scripts/pi-hole/js/list.js
+++ static/scripts/pi-hole/js/list.js
@@ -3,98 +3,22 @@ $.ajaxSetup({
     cache: false
 });
 // Get PHP info
-var token = $("#token")
-    .html();
-var listType = $("#list-type")
-    .html();
-var fullName = listType === "white" ? "Whitelist" : "Blacklist";
+var domainList = {};
+(function(dList) {
+    const token = $("#token")
+        .html();
+    const listType = $("#list-type")
+        .html();
+    const fullName = listType === "white" ? "Whitelist" : "Blacklist";
+    const alInfo = $("#alInfo");
+    const alSuccess = $("#alSuccess");
+    const alFailure = $("#alFailure");
+    const domain = $("#domain");
+    const list = $("#list");
 
-function sub(index, entry) {
-    var domain = $("#" + index);
-    domain.hide("highlight");
-    $.ajax({
-        url: "scripts/pi-hole/php/sub.php",
-        method: "post",
-        data: {
-            "domain": entry,
-            "list": listType,
-            "token": token
-        },
-        success: function(response) {
-            if (response.length !== 0) {
-                return;
-            }
-            domain.remove();
-        },
-        error: function(jqXHR, exception) {
-            domain.show({
-                queue: true
-            });
-            alert("Failed to remove the domain!");
-        }
-    });
-}
-
-function refresh(fade) {
-    var list = $("#list");
-    if (fade) {
-        list.fadeOut(100);
-    }
-    $.ajax({
-        url: "/api/list",
-        method: "get",
-        data: {
-            "list": listType
-        },
-        success: function(data) {
-            list.html("");
-            if (data.length === 0) {
-                list.html("<div class=\"alert alert-info\" role=\"alert\">Your " + fullName + " is empty!</div>");
-            } else {
-                data.forEach(function(entry, index) {
-                    list.append(
-                        "<li id=\"" + index + "\" class=\"list-group-item clearfix\">" + entry +
-                        "<button class=\"btn btn-danger btn-xs pull-right\" type=\"button\">" +
-                        "<span class=\"glyphicon glyphicon-trash\"></span></button></li>"
-                    );
-                    // Handle button
-                    $("#list #" + index + "")
-                        .on("click", "button", function() {
-                            sub(index, entry);
-                        });
-                });
-            }
-            list.fadeIn("fast");
-        },
-        error: function(jqXHR, exception) {
-            $("#alFailure")
-                .show();
-        }
-    });
-}
-window.onload = refresh(false);
-
-function add() {
-    var domain = $("#domain");
-    if (domain.val()
-        .length === 0) {
-        return;
-    }
-    var alInfo = $("#alInfo");
-    var alSuccess = $("#alSuccess");
-    var alFailure = $("#alFailure");
-    alInfo.show();
-    alSuccess.hide();
-    alFailure.hide();
-    $.ajax({
-        url: "scripts/pi-hole/php/add.php",
-        method: "post",
-        data: {
-            "domain": domain.val(),
-            "list": listType,
-            "token": token
-        },
-        success: function(response) {
+    //Callbacks
+    const addCallbacks = {
+        "success": function(response) {
             if (response.indexOf("not a valid argument") >= 0 ||
                 response.indexOf("is not a valid domain") >= 0) {
                 alFailure.show();
@@ -117,10 +41,10 @@ function add() {
                         alInfo.hide();
                     });
                 domain.val("");
-                refresh(true);
+                dList.refresh(true);
             }
         },
-        error: function(jqXHR, exception) {
+        "error": function(jqXHR, exception) {
             alFailure.show();
             alFailure.delay(1000)
                 .fadeOut(2000, function() {
@@ -131,25 +55,117 @@ function add() {
                     alInfo.hide();
                 });
         }
-    });
-}
+    };
+    const subCallbacks = {
+        "error": function(jqXHR, exception) {
+            domain.show({
+                queue: true
+            });
+            alert("Failed to remove the domain!");
+        },
+        "success": function(response) {
+            if (response.length !== 0) {
+                return;
+            }
+            domain.remove();
+        }
+    };
+    const refreshCallbacks = {
+        "success": function(data) {
+            list.html("");
+            if (data.length === 0) {
+                list.html("<div class=\"alert alert-info\" role=\"alert\">Your " + fullName + " is empty!</div>");
+            } else {
+                data.forEach(function(entry, index) {
+                    list.append(
+                        "<li id=\"" + index + "\" class=\"list-group-item clearfix\">" + entry +
+                        "<button class=\"btn btn-danger btn-xs pull-right\" type=\"button\">" +
+                        "<span class=\"glyphicon glyphicon-trash\"></span></button></li>"
+                    );
+                    // Handle button
+                    $("#list #" + index + "")
+                        .on("click", "button", function() {
+                            dList.sub(index, entry);
+                        });
+                });
+            }
+            list.fadeIn("fast");
+        },
+        "error": function(jqXHR, exception) {
+            $("#alFailure")
+                .show();
+        }
+    };
+
+    dList.add = function() {
+        if (domain.val()
+            .length === 0) {
+            return;
+        }
+        alInfo.show();
+        alSuccess.hide();
+        alFailure.hide();
+        $.ajax({
+            url: "scripts/pi-hole/php/add.php",
+            method: "post",
+            data: {
+                "domain": domain.val(),
+                "list": listType,
+                "token": token
+            },
+            success: addCallbacks.success,
+            error: addCallbacks.error
+        });
+    };
+    dList.sub = function(index, entry) {
+        domain.hide("highlight");
+        $.ajax({
+            url: "scripts/pi-hole/php/sub.php",
+            method: "post",
+            data: {
+                "domain": entry,
+                "list": listType,
+                "token": token
+            },
+            success: subCallbacks.success,
+            error: subCallbacks.error
+        });
+    };
+    dList.refresh = function(fade) {
+        if (fade) {
+            list.fadeOut(100);
+        }
+        $.ajax({
+            url: "/api/list",
+            method: "get",
+            data: {
+                "list": listType
+            },
+            success: refreshCallbacks.success,
+            error: refreshCallbacks.error
+        });
+    }
+}(domainList));
+
+window.onload = domainList.refresh(false);
+
 // Handle enter button for adding domains
 $(document)
     .keypress(function(e) {
         if (e.which === 13 && $("#domain")
             .is(":focus")) {
             // Enter was pressed, and the input has focus
-            add();
+            domainList.add();
         }
     });
 // Handle buttons
 $("#btnAdd")
     .on("click", function() {
-        add();
+        domainList.add();
     });
 $("#btnRefresh")
     .on("click", function() {
-        refresh(true);
+        domainList.refresh(true);
     });
 // Handle hiding of alerts
 $(function() {
