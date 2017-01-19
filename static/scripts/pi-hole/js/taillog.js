e8723f8c8abe4709d84920d857509d3eafc54e2d
wdev733 <wdev733@gmail.com>
2017-01-19 01:56:09 +0100

    Modularized taillog.js

--- static/scripts/pi-hole/js/taillog.js
+++ static/scripts/pi-hole/js/taillog.js
@@ -1,46 +1,27 @@
-var offset, timer, pre, scrolling = true,
-    token, evSource;
-$(function() {
-    token = $("token")
-        .html();
-    pre = $("#output");
-    /*evSource = new EventSource("/api/taillog", {
-        "withcredentials": true
-    });
-    evSource.onerror = function(err) {
-        console.log("onerror");
-    };
-    evSource.onmessage = function(data) {
-        console.log(data);
-    };
-    evSource.addEventListener("dns", function(e) {
-        pre.append(e.data);
-        if (scrolling) {
-            window.scrollTo(0, document.body.scrollHeight);
-        }
-    }, false);
-    evSource.onopen = function() {
-        console.log("on open");
-    };*/
+/* globals taillogWatcher */
 
+(function() {
+    var scrolling = true;
+    const token = $("token")
+        .html();
+    const pre = $("#output");
+    const chk1 = $("#chk1");
+    const chk2 = $("#chk2");
+    const chkCallback = function() {
+        if ($(this).is(chk1)) {
+            chk2.prop("checked", this.checked);
+        } else if ($(this).is(chk2)) {
+            chk1.prop("checked", this.checked);
+        }
+        scrolling = this.checked;
+    };
+    chk1.click(chkCallback);
+    chk2.click(chkCallback);
     taillogWatcher
         .on("dns", function(data) {
-            console.log(data);
-            pre.append(data);
+            pre.append(data.timestamp);
             if (scrolling) {
                 window.scrollTo(0, document.body.scrollHeight);
             }
         });
-});
-$("#chk1")
-    .click(function() {
-        $("#chk2")
-            .prop("checked", this.checked);
-        scrolling = this.checked;
-    });
-$("#chk2")
-    .click(function() {
-        $("#chk1")
-            .prop("checked", this.checked);
-        scrolling = this.checked;
-    });
+}());
