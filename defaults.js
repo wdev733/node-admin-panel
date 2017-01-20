7af8397453dd3075bde26fbb7a2f0bce53fdc822
wdev733 <wdev733@gmail.com>
2017-01-20 17:59:36 +0100

    added actual file paths to gravitListFile

--- defaults.js
+++ defaults.js
@@ -11,7 +11,7 @@ if (process.env.NODE_ENV !== "test") {
         port: 3000,
         jwtSecret: "236761d24ce20e1c6dde676068b52d6453bd3b25dccce51b4ddb6cd78bb1a557",
         cookieSecret: "c7f88711ba33773fee39fada7503f3f704d308f25a34987eb6a1b79f21c51ee0",
-        gravityListFile: ""
+        gravityListFile: "/etc/pihole/list.preEventHorizon"
     };
 } else {
     module.exports = {
@@ -22,6 +22,6 @@ if (process.env.NODE_ENV !== "test") {
         port: 3000,
         jwtSecret: "236761d24ce20e1c6dde676068b52d6453bd3b25dccce51b4ddb6cd78bb1a557",
         cookieSecret: "c7f88711ba33773fee39fada7503f3f704d308f25a34987eb6a1b79f21c51ee0",
-        gravityListFile: ""
+        gravityListFile: __dirname + "/test/list.preEventHorizon"
     };
 }
