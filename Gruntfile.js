a9f8e4280ea3b8c6e1164a8d6947772c5eb58661
wdev733 <wdev733@gmail.com>
2017-01-24 15:45:20 +0100

    Added js beautifier to grunt task

--- Gruntfile.js
+++ Gruntfile.js
@@ -7,11 +7,31 @@ module.exports = function(grunt) {
                 src: "./server/",
                 dest: "./docs/"
             }
+        },
+        jsbeautifier: {
+            files: ["server/**/*.js",
+                "server/**/*.json",
+                "!node_modules/**",
+                "!server/**/vendor/**"
+            ],
+            options: {
+                js: {
+                    breakChainedMethods: true,
+                    indentChar: " ",
+                    indentSize: 4,
+                    indentWithTabs: false,
+                    jslintHappy: false,
+                    maxPreserveNewlines: 2,
+                    preserveNewlines: true,
+                    endWithNewline: true
+                }
+            }
         }
     });
 
     grunt.loadNpmTasks('grunt-apidoc');
+    grunt.loadNpmTasks("grunt-jsbeautifier");
 
-    grunt.registerTask('default', ['apidoc']);
+    grunt.registerTask('default', ['apidoc', "jsbeautifier"]);
 
 };
