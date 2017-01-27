a61f87f6bcb19429c4ea5939b7604ece51b1cee8
wdev733 <wdev733@gmail.com>
2017-01-27 14:59:23 +0100

    Remove taillog

--- server/server.js
+++ server/server.js
@@ -93,14 +93,6 @@ PiServer.prototype.start = function() {
                 console.log("Server listening on port " + appDefaults.port + "!");
             }
             .bind(this));
-        var tail = new Tail(appDefaults.logFile);
-        tail.on("line", function(data) {
-            const lineInfo = logHelper.parseLine(data);
-            if (lineInfo === false) {
-                return;
-            }
-        }.bind(this));
-        tail.on("error", function(error) {});
     }
 };
 module.exports = PiServer;
